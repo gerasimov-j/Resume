6af831d556996c300b456d741912c8d80c8563c8
Gerasimov Jegor <herasimau.jahor@gmail.com>
2020-03-27 19:31:52 +0300

    oracle create

--- src/main/java/utils/QueryManager.java
+++ src/main/java/utils/QueryManager.java
@@ -6,9 +6,9 @@ import java.nio.file.Paths;
 
 public class QueryManager {
 
-    private static final String CREATE_PATH = System.getProperty("user.dir") + "/src/main/resources/sql/create.sql";
-    private static final String INSERT_PATH = System.getProperty("user.dir") + "/src/main/resources/sql/insert.sql";
-    private static final String DROP_PATH = System.getProperty("user.dir") + "/src/main/resources/sql/drop.sql";
+    private static final String CREATE_PATH = System.getProperty("user.dir") + "/src/main/resources/oracle/create.sql";
+    private static final String INSERT_PATH = System.getProperty("user.dir") + "/src/main/resources/oracle/insert.sql";
+    private static final String DROP_PATH = System.getProperty("user.dir") + "/src/main/resources/oracle/drop.sql";
 
     private static String getQuery(String path) {
         StringBuilder result = new StringBuilder();
